56f2050a98780957c836fae165569cc68f84b8e9
ThongND <thongngo3301@gmail.com>
2018-11-01 00:50:10 +0700

    create login-form component

--- cs-client/src/app/app.module.ts
+++ cs-client/src/app/app.module.ts
@@ -1,11 +1,13 @@
 // Import modules
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { createCustomElement } from '@angular/elements';
 
 // Import components
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SurveySheetComponent } from './components/survey-sheet/survey-sheet.component';
 
 // Import services
@@ -14,7 +16,8 @@ import { CsApiService } from './services/cs-api.service';
 @NgModule({
   declarations: [
     AppComponent,
-    SurveySheetComponent
+    SurveySheetComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +27,21 @@ import { CsApiService } from './services/cs-api.service';
   providers: [
     CsApiService
   ],
-  bootstrap: [AppComponent]
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  entryComponents: [
+    LoginFormComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const loginForm = createCustomElement(LoginFormComponent, {
+      injector
+    });
+    customElements.define('login-form', loginForm);
+  }
+}
