321f6d184ab10a415284d4ce915aa8ae89677401
ThongND <thongngo3301@gmail.com>
2018-11-01 17:32:58 +0700

    init survey-sheet

--- cs-client/src/app/app-routing.module.ts
+++ cs-client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HomeComponent } from './components/home/home.component';
+import { SurveySheetComponent } from './components/survey-sheet/survey-sheet.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'survey', component: SurveySheetComponent },
   { path: '**', redirectTo: '' }
 ];
 
